8558ff6eb22dcda5d069e5ab6f82462cfa8db0c6
Dragoș Roșcuț <dragos.roscut@endava.com>
2024-12-11 16:14:23 +0200

    Functions as Types

--- app.ts
+++ app.ts
@@ -7,3 +7,13 @@ function printResult(num: number): void {
 }
 
 printResult(add(5, 12));
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult; // commenting because printResult takes one parameter and returns void
+// combineValues = 5;
+
+console.log(combineValues(8, 8));
+
+//let someValue: undefined;
