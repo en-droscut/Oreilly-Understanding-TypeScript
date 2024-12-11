c6434c9746913e6d45d12acdb047ac24fee9891c
Dragoș Roșcuț <dragos.roscut@endava.com>
2024-12-11 16:39:01 +0200

    The 'never' type

--- app.ts
+++ app.ts
@@ -8,3 +8,11 @@ if (typeof userInput === "string") {
   // without this extra check it will not work because the userInput is 'unknown'
   userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+
+  // while (true) {} // also a function that has an infinte loop NEVER returns anything
+}
+
+generateError("An error occured", 500);
